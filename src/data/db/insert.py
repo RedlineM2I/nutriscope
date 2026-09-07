0fc80adb52cebcbb6c7648bef51fc4bf7d1ecf8b
Nathan Marquis <nathan.marquis.pro@gmail.com>
2026-09-07 13:36:03 +0200

    refactor : modification des noms de variables

--- src/data/db/insert.py
+++ src/data/db/insert.py
@@ -5,7 +5,7 @@ from _duckdb import DuckDBPyConnection
 from sqlalchemy import Connection
 
 from src.data.db.connection import get_engine
-from src.data.extract.tags import build_liaison_table
+from src.data.extract.tags import build_link_table
 
 #Tables où il faut créer une table de liaison
 TAG_TABLES = {
@@ -25,36 +25,36 @@ def insert_all_in_db(conn_duckdb : DuckDBPyConnection) :
     engine = get_engine()
     try:
         with engine.begin() as conn:  # commit automatique si succès, rollback si erreur
-            insert_produits(get_produits(conn_duckdb=conn_duckdb), conn)
+            insert_products(get_produits(conn_duckdb=conn_duckdb), conn)
             for source_col, table_name in TAG_TABLES.items():
                 print(f"Insertion des {table_name}")
-                table, table_liaison = build_liaison_table(conn_duckdb, source_col, "nom", table_name)
-                insert_in_db_copy(table, table_name, conn)
-                insert_in_db_copy(table_liaison, f"produits_{table_name}", conn)
+                id_tag_nm_table, link_table = build_link_table(conn_duckdb, source_col, "nom", table_name)
+                insert_in_db_copy(id_tag_nm_table, table_name, conn)
+                insert_in_db_copy(link_table, f"produits_{table_name}", conn)
             #insert_nutriments(con, conn)
     except Exception as e:
         logger.error(f"Échec de l'import : {e}")
 
-def insert_in_db_copy(df: pd.DataFrame, table_name: str, conn : Connection, colonnes_int: list[str] = None) :
+def insert_in_db_copy(df: pd.DataFrame, table_name: str, conn : Connection, columns_int: list[str] = None) :
     df = df.copy()
-    for col in colonnes_int or []:
+    for col in columns_int or []:
         df[col] = df[col].astype("Int64")
 
     buffer = io.StringIO()
     df.to_csv(buffer, index=False, header=False, na_rep="\\N")
     buffer.seek(0)
 
-    colonnes_sql = f"({', '.join(df.columns)})"
+    columns_sql = f"({', '.join(df.columns)})"
     cur = conn.connection.cursor()
     cur.copy_expert(
-        f"COPY {table_name} {colonnes_sql} FROM STDIN WITH (FORMAT csv, NULL '\\N')",
+        f"COPY {table_name} {columns_sql} FROM STDIN WITH (FORMAT csv, NULL '\\N')",
         buffer
     )
     logger.info(f"{len(df)} lignes importées avec succès")
 
-def insert_produits(df : pd.DataFrame, conn_psql : Connection):
+def insert_products(df : pd.DataFrame, conn_psql : Connection):
     print("Insertion des produits")
-    insert_in_db_copy(df, "produits", conn_psql, colonnes_int=["nova_group", "nutriscore_score", "environmental_score_score"])
+    insert_in_db_copy(df, "produits", conn_psql, columns_int=["nova_group", "nutriscore_score", "environmental_score_score"])
 
 def get_produits(conn_duckdb : DuckDBPyConnection) :
     query = f"""
