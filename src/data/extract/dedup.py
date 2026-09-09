70a9c2596fe5367154d82db890d2c0271c375305
Nathan Marquis <nathan.marquis.pro@gmail.com>
2026-09-09 16:14:33 +0200

    fix : import du chemin du fichier parquet depuis config

--- src/data/extract/dedup.py
+++ src/data/extract/dedup.py
@@ -1,10 +1,10 @@
-FOOD_FR_PARQUET = "../../data/food_france.parquet"
+from src.config import PARQUET_FR
 
 def create_produits_code_clean(conn) :
     conn.sql(f"""
         CREATE OR REPLACE TEMP TABLE produits_dedup AS
         SELECT *
-        FROM '{FOOD_FR_PARQUET}'
+        FROM '{PARQUET_FR}'
         QUALIFY ROW_NUMBER() OVER (PARTITION BY code ORDER BY completeness DESC) = 1
     """)
 
